c6d01afc72e77334cb5a0f764cf30ff050d6db61
iashjan <ashjanot@gmail.com>
2019-10-14 14:14:36 +0300

    tic tac

--- js/main.js
+++ js/main.js
@@ -1,17 +1,84 @@
 
+var currentState=[];
+var winnerInex=[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
+]
+var table = document.querySelector(".table-container");
 
 
-function myCreateFunction() {
+
+// function mainGame () {
+
+  function myCreateFunction() {
     for (let i = 0; i <9;  i++){
-        var table = document.querySelector(".table-container");
-
-        var box = document.createElement("div");
+      var box = document.createElement("div");
         box.classList.add("box");
-
         table.appendChild(box);
+        box.setAttribute("id", i)
+        box.addEventListener("click",player);
+        currentState.push('');
+        // box.innerText="x";
 
     }
   }
 
-
   myCreateFunction()
+
+// function ready(){
+//       let plalyer1 = X
+//       let computer = o
+//       let result = ""
+      
+
+//   }  
+  
+  function player(){
+  if(this.innerText ===''){
+    this.innerText="x";
+    currentState[this.getAttribute("id")]="x";
+    if( checkForWinner()){
+      alert("you win game ended")
+    }
+    else{
+      computer();
+    }
+  }
+  }
+
+
+  function computer(){
+    let randomIndex=Math.floor(Math.random()*9);
+    while(currentState[randomIndex]!=''){
+      randomIndex=Math.floor(Math.random()*9);
+    }
+    document.getElementById(randomIndex).innerText="o";
+    currentState[randomIndex]="o"
+    if(checkForWinner()){
+      alert("you lose game ended")
+    }
+  }
+
+  function checkForWinner(){
+    for(let i=0;i<winnerInex.length;i++){
+      if(currentState[winnerInex[i][0]]!=''&&currentState[winnerInex[i][0]]==currentState[winnerInex[i][1]]&&currentState[winnerInex[i][1]]  == currentState[winnerInex[i][2]]  ){
+
+        return true;
+
+      }
+       
+
+  }
+  return false;
+}
+
+
+
+
+// mainGame()
